674b920acb2117d5d297d0497808e30c6c629f42
KirillShet <146167926+KirillShet@users.noreply.github.com>
2023-10-24 13:03:03 +0300

    Add files via upload

--- ConsoleApp2/pyramid.cs
+++ ConsoleApp2/pyramid.cs
@@ -13,17 +13,24 @@ namespace ConsoleApp1
         private bool pov;
         public void out_info(float a1, float h1, float n1, float r1)
         {
-            if (r1 == 0)
+            if (r1 == 0 && a1 == 0 && n1 == 0)
             {
-                Console.WriteLine("Периметр пирамидки: " + perimeter3(a1, h1, n1));
-                Console.WriteLine("Площадь пирамидки: " + square3(a1, h1, n1));
-                Console.WriteLine("Объем пирамиды равен: " + vich(a1, h1, n1, r1));
+                Console.WriteLine();
             }
-            else if (r1 > 0)
+            else
             {
-                Console.WriteLine("Периметр пирамидки: " + perimeter5(r1));
-                Console.WriteLine("Площадь пирамидки: " + square5(h1, r1));
-                Console.WriteLine("Объем пирамиды равен: " + vich2(h1, r1));
+                if (r1 == 0)
+                {
+                    Console.WriteLine("Периметр пирамидки: " + perimeter3(a1, h1, n1));
+                    Console.WriteLine("Площадь пирамидки: " + square3(a1, h1, n1));
+                    Console.WriteLine("Объем пирамиды равен: " + vich(a1, h1, n1, r1));
+                }
+                else if (r1 > 0)
+                {
+                    Console.WriteLine("Периметр пирамидки: " + perimeter5(r1));
+                    Console.WriteLine("Площадь пирамидки: " + square5(h1, r1));
+                    Console.WriteLine("Объем пирамиды равен: " + vich2(h1, r1));
+                }
             }
         }
     }
